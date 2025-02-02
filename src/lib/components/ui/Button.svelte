ac0623f0bbe6aa03cb74c7b5be06c86542d107cc
sonjahanekom <firebridfrankie13@gmail.com>
2025-02-02 23:56:59 +0000

    Add Nav Bar

--- src/lib/components/ui/Button.svelte
+++ src/lib/components/ui/Button.svelte
@@ -8,24 +8,24 @@
 	}
 	let { children, href }: Props = $props();
 
-	let classList = [
+	let classList = $derived([
 		' btn border-2 transition-all ',
 		page.url.pathname !== href &&
 			'bg-surface-50 hover:translate-x-1 hover:translate-y-1.5 hover:shadow-none border-surface-950 text-surface-950',
 		page.url.pathname === href &&
 			'interactive-current translate-x-1 translate-y-1.5 border-primary-950 bg-primary-50 text-primary-950'
-	];
+	]);
 
 	let bWidth = $state();
 	let bHeight = $state();
 </script>
 
-<div class="size-fit pb-1.5 pr-1">
+<div class="flex size-fit pb-1.5 pr-1">
 	<a {href} type="button" class={classList} bind:offsetWidth={bWidth} bind:offsetHeight={bHeight}>
 		{@render children()}
 	</a>
 	<div
 		style="width: {bWidth}px; height: {bHeight}px;"
-		class="btn absolute left-0 -z-10 translate-x-1 translate-y-1.5 bg-surface-950"
+		class="btn absolute -z-10 translate-x-1 translate-y-1.5 bg-surface-950"
 	></div>
 </div>
---
+++ src/lib/components/ui/Nav.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Button from '$lib/components/ui/Button.svelte';
+	import { House, PencilRuler, Rss, Search, User } from 'lucide-svelte';
+</script>
+
+<div
+	class="absolute left-0 top-1/2 flex size-fit -translate-y-1/2 flex-col gap-2 rounded-br-3xl rounded-tr-3xl border-b-2 border-l-0 border-r-2 border-t-2 border-surface-950 p-4"
+>
+	<Button href="/"><House /></Button>
+	<Button href="/blog"><Rss /></Button>
+	<Button href="/tools"><PencilRuler /></Button>
+	<Button href="/about-me"><User /></Button>
+	<Button href="/search"><Search /></Button>
+</div>
