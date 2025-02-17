b0aa8cc778b024b6f695ca132c31014c8c690dc5
Sonja Hanekom <120342687+SonjaHanekom@users.noreply.github.com>
2025-02-17 21:42:10 +0000

    Styling (#17)
    * Add Grid background styling
    
    * Fdd fonts
    
    ---------
    
    Co-authored-by: sonjahanekom <firebridfrankie13@gmail.com>

--- src/lib/components/ui/Nav.svelte
+++ src/lib/components/ui/Nav.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div
-	class="absolute left-0 top-1/2 flex size-fit -translate-y-1/2 flex-col gap-2 rounded-br-3xl rounded-tr-3xl border-b-2 border-l-0 border-r-2 border-t-2 border-surface-950 p-4"
+	class="absolute left-0 top-1/2 flex size-fit -translate-y-1/2 flex-col gap-2 rounded-br-3xl rounded-tr-3xl border-b-2 border-l-0 border-r-2 border-t-2 border-surface-950 bg-surface-50 p-4"
 >
 	<Button href="/"><House /></Button>
 	<Button href="/blog"><Rss /></Button>
